367ea0f5026111048d32a514d7d0bbfc23245a26
Farzod <farzodachililov27@gmail.com>
2023-03-02 13:02:00 +0500

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import GuessTheNumber from "./companets/game/game";
+import GuessTheNumber from "./companets/game/Game";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
